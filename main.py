0525dffc8d594ef61722d02d8dbdb5521618ca4a
sakup <pukasbohdan@gmail.com>
2016-11-23 19:19:17 +0200

    ready to play

--- main.py
+++ main.py
@@ -66,11 +66,11 @@ class PongGame(Widget):
             self.player1.score += 1
             self.serve_ball(vel = (-4, 0))
 
-        def on_touch_move(self, touch):
-            if touch.x < self.width / 3:
-                self.player1.center_y = touch.y
-            if touch.x > self.width  - self.width / 3:
-                self.player2.center_y = touch.y
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width  - self.width / 3:
+            self.player2.center_y = touch.y
 
 
 
