07f7a1aea59db246a22e5450c0a2ac482ea3e25c
sakup <pukasbohdan@gmail.com>
2016-11-23 19:01:39 +0200

    add paddles, and some logic

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ from random import randint
 kivy.config.Config.set('graphics', 'width', '800')
 kivy.config.Config.set('graphics', 'height', '450')
 
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
+
 class PongBall(Widget):
 
     # velocity of the ball on x and y axis
@@ -24,21 +36,41 @@ class PongBall(Widget):
 
 
 class PongGame(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
-    def serve_ball(self):
+    def serve_ball(self,vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         # call ball.move and other stuff
         self.ball.move()
+        # bounce of paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
         # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
-
         # bounce off left and right
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
+        # if (self.ball.x < 0) or (self.ball.right > self.width):
+        #     self.ball.velocity_x *= -1
+
+        # score points
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel = (4, 0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel = (-4, 0))
+
+        def on_touch_move(self, touch):
+            if touch.x < self.width / 3:
+                self.player1.center_y = touch.y
+            if touch.x > self.width  - self.width / 3:
+                self.player2.center_y = touch.y
 
 
 
