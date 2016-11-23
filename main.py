2e759d4e902885132b896ccba08b2850bcd0e732
sakup <pukasbohdan@gmail.com>
2016-11-23 13:22:01 +0200

    add serve_ball and update methods

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
-class PongGame(Widget):
-    pass
+kivy.config.Config.set('graphics', 'width', '800')
+kivy.config.Config.set('graphics', 'height', '450')
 
 class PongBall(Widget):
 
@@ -20,10 +23,32 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity) + self.pos
 
 
+class PongGame(Widget):
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        # call ball.move and other stuff
+        self.ball.move()
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
+
+
 class PongApp(App):
 
     def build(self):
-        return PongGame()
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 if __name__ == '__main__':
     PongApp().run()
